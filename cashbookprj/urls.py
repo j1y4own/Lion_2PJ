cd002b590f965e8ed8d49bc28697772c73e26e75
jiyun <missji1004@gmail.com>
2022-10-09 18:48:22 +0900

    유효성 검사

--- cashbookprj/urls.py
+++ cashbookprj/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', cashbookapp.views.main, name='main'),
+    path('read', cashbookapp.views.read, name='read'),
     path('',include('cashbookapp.urls')),
     path('',include('cuapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
